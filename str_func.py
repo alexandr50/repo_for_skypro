c0b43be44cda9444574cc9c1786fbb43311eada0
alexandr50 <dmitrymaslov2016@mail.ru>
2023-02-12 13:52:59 +0300

    change str_func.py

--- str_func.py
+++ str_func.py
@@ -1,5 +1,10 @@
 
 
 def func_upper(text: str):
+    """"Функция делающая первые буквы заглавными"""
     return text.upper()
 
+
+def func_title(text: str):
+    """Функция преобразовывающая все буквы в заглавные """
+    return ' '.join([x.title() for x in text.split(' ')])
